245b7a1ea875190900ee09e9596362006aa05df8
apotinvs <apotinvs@gmail.com>
2020-10-20 22:00:39 +0600

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	while(str){
+        let flag = false
+        for(const i of bracketsConfig) {
+            let brackets = i[0] + i[1]
+            if(str.includes(brackets)){
+                str = str.replace(brackets, '')
+                flag = true
+            }
+        }
+        if(!flag)
+            break;
+    }
+    return str == '' 
 }
